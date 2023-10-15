a71156f3d7911d0552cb1b765f5c5e4422b0c683
Попкова Дарья <daryapopkova90@yandex.ru>
2023-10-16 01:07:39 +0300

    после ревью

--- blogicum/blog/models.py
+++ blogicum/blog/models.py
@@ -13,10 +13,10 @@ class Category(BaseModel):
     title = models.CharField(
         max_length=256,
         verbose_name='Заголовок'
-        )
+    )
     description = models.TextField(
         verbose_name='Описание'
-        )
+    )
     slug = models.SlugField(
         unique=True,
         verbose_name='Идентификатор',
@@ -25,7 +25,7 @@ class Category(BaseModel):
             'разрешены символы латиницы, цифры, '
             'дефис и подчёркивание.'
             )
-                   )
+    )
 
     class Meta:
         verbose_name = 'категория'
@@ -42,7 +42,7 @@ class Location(BaseModel):
     name = models.CharField(
         max_length=256,
         verbose_name='Название места'
-        )
+    )
 
     class Meta:
         verbose_name = 'местоположение'
@@ -71,20 +71,20 @@ class Post(BaseModel):
         User,
         on_delete=models.CASCADE,
         verbose_name='Автор публикации',
-        )
+    )
     location = models.ForeignKey(
         Location,
         on_delete=models.SET_NULL,
         blank=True,
         null=True,
         verbose_name='Местоположение',
-        )
+    )
     category = models.ForeignKey(
         Category,
         on_delete=models.SET_NULL,
         null=True,
         verbose_name='Категория',
-        )
+    )
     image = models.ImageField(
         upload_to='posts_images/',
         blank=True
@@ -108,16 +108,16 @@ class Comment(models.Model):
         Post,
         on_delete=models.CASCADE,
         related_name='comments'
-        )
+    )
     author = models.ForeignKey(
         User,
         on_delete=models.CASCADE,
         related_name='comments'
-        )
+    )
     created_at = models.DateTimeField(
         auto_now_add=True,
         verbose_name='Дата и время публикации'
-        )
+    )
 
     class Meta:
         verbose_name = 'комментарий'
